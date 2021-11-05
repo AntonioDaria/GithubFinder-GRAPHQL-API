0c0b36bc7cfe86fa2f6ef983fcb8760a1ba8964b
Antonio D'aria <antoniodaria@msn.com>
2021-11-05 15:23:34 +0000

    implemented function to determinefavourite language

--- resolvers/utils.js
+++ resolvers/utils.js
@@ -4,9 +4,37 @@ const axios = require('axios').default;
 const githubApiCall = async (userName) => {
   const url = `https://api.github.com/users/${userName}/repos`;
   const result = await axios.get(url);
-  console.log("AAA", result.data);
+  //console.log("AAA", result.data);
   return result.data;
 };
 
+const extractFavLanguage = (ArrayOfLanguages) => {
 
-module.exports = { githubApiCall }
+    let languageMap = {};
+    let value = '';
+    let maxCount = 0;
+    let result = {}
+    for (let language of ArrayOfLanguages) {
+
+        if (languageMap[language] == null) {
+            languageMap[language] = 1;
+        } else {
+            languageMap[language]++;
+        }
+
+        if (languageMap[language] > maxCount) {
+            value = language;
+            maxCount = languageMap[language];
+        }
+    }
+    return (
+        result = {
+            name: value,
+            numbOfRepo: maxCount,
+        }
+    );
+}
+
+
+
+module.exports = { githubApiCall, extractFavLanguage }
